4fc6e1c23a6f6faacfe65f5caf7acf6de5da0008
arahito1994 <artht0408@gmail.com>
2022-09-09 05:38:19 +0900

    useContext

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import './App.css'
+import ShinCodeContext from './main';
 
 function App() {
   const [count, setCount] = useState(0);
+  const shincodeInfo = useContext(ShinCodeContext);
 
   const handleClick = () => {
     setCount(count + 1);
@@ -17,6 +19,11 @@ function App() {
       <h1>useState, useEffect</h1>
       <button onClick={handleClick}>+</button>
       <p>{count}</p>
+
+      <hr />
+      <h1>useContext</h1>
+      <h2>{shincodeInfo.name}</h2>
+      <h2>{shincodeInfo.age}</h2>
     </div>
   )
 }
