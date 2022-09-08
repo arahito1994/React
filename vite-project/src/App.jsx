48847bbcca7cc8923183554fc121c38707dcd71c
arahito1994 <artht0408@gmail.com>
2022-09-09 06:40:24 +0900

    useReducer

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,23 @@
-import { useEffect, useState, useContext, useRef } from 'react';
+import { useEffect, useState, useContext, useRef, useReducer } from 'react';
 import './App.css'
 import ShinCodeContext from './main';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      return state;
+  }
+};
 
 function App() {
   const [count, setCount] = useState(0);
   const shincodeInfo = useContext(ShinCodeContext);
   const ref =useRef();
+  const [state, dispatch] = useReducer(reducer, 0);
 
   const handleClick = () => {
     setCount(count + 1);
@@ -36,8 +48,13 @@ function App() {
       <input type="text" ref ={ref}/>
       <button onClick={handleRef}>useRef</button>
 
+      <hr />
+      <h1>useReducer</h1>
+      <p>カウント:{state}</p>
+      <button onClick={() => dispatch({type: "increment" })}>+</button>
+      <button onClick={() => dispatch({type: "decrement" })}>-</button>
     </div>
-  )
+  );
 }
 
 export default App
