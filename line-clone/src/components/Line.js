4e26f9697252b32178ddb5348565c2ee06481abc
arahito1994 <artht0408@gmail.com>
2022-09-18 11:30:56 +0900

    外観調整

--- line-clone/src/components/Line.js
+++ line-clone/src/components/Line.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import SignOut from './SignOut'
-import { db } from '../firebase';
+import { auth, db } from '../firebase';
 import SendMessage from './SendMessage';
 
 function Line() {
@@ -20,7 +20,7 @@ function Line() {
       <div className="msgs">
         {messages.map(({id, text, photoURL, uid}) => (
           <div>
-            <div key={id}>
+            <div key={id}　className={`msg ${uid === auth.currentUser.uid ? "sent" : "received"}`}>
               <img src={photoURL} alt="" />
               <p>{text}</p>
             </div>
