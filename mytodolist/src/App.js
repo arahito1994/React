048fc63f1d8791e93f10a5034fa8735566fd1a42
arahito1994 <artht0408@gmail.com>
2023-05-28 10:05:23 +0900

    コンポーネントの作成

--- mytodolist/src/App.js
+++ mytodolist/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import InputForm from './Components/InputForm';
+import Title from './Components/Title';
+import TodoList from './Components/TodoList';
+
 
 function App() {
   return (
     <div>
-      
+       {/* 必要なコンポーネントを作成 */}
+      <Title />
+      <InputForm />
+      <TodoList />
     </div>
   );
 }
---
+++ mytodolist/src/Components/Title.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <h1>My ToDo List</h1>
+  )
+}
+
+export default Title
