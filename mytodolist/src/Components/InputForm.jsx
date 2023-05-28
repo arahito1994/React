8e21f3f0ac36054acfc70577f3e9dc8cc10f2454
arahito1994 <artht0408@gmail.com>
2023-05-28 23:16:25 +0900

    inputに入力した文字の取得

--- mytodolist/src/Components/InputForm.jsx
+++ mytodolist/src/Components/InputForm.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { useState } from 'react'
+
+// handleSubmitの定義
+const handleSubmit = (e) => {
+  e.preventDefault();
+  
+  // inputに入力した文字を表示したい
+}
+
+const handleChange = (e) => {
+  let inputText = e.target.value;
+  console.log(inputText);
+}
 
 const InputForm = () => {
   return (
     <div>
-      <form>
-        <input></input>
+      {/* フォームに入力した文字を取得する */}
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleChange}></input>
         <button>投稿用ボタン</button>
         <button>削除用ボタン</button>
       </form>
