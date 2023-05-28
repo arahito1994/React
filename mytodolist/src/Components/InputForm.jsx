77b933adf6c3ad21c476456a51c43c17d1e9eac9
arahito1994 <artht0408@gmail.com>
2023-05-28 11:49:54 +0900

    投稿用のフォームを作成

--- mytodolist/src/Components/InputForm.jsx
+++ mytodolist/src/Components/InputForm.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const InputForm = () => {
   return (
-    <div>InputForm</div>
+    <div>
+      <form>
+        <input></input>
+        <button>投稿用ボタン</button>
+        <button>削除用ボタン</button>
+      </form>
+    </div>
   )
 }
 
