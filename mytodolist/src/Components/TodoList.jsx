fc3f60dad0617584a64ea985ba9d798a7e8c5830
arahito1994 <artht0408@gmail.com>
2023-06-01 23:12:41 +0900

    入力した文字の削除

--- mytodolist/src/Components/TodoList.jsx
+++ mytodolist/src/Components/TodoList.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 
 const TodoList = ({ taskList, setTaskList }) => {
+
+  const handleDelete = () => {
+    
+  }
+
   return (
     // todoを表示するエリアを作成する
     <div className='todos'>
       <div className='todos'>
-        {taskList.map((task, index)=> (
+        {taskList.map((task, index) => (
           <div className='todo' key={index}>
           <div className='todoText'>
           <p>  
             <input type='checkbox'></input>
               <span>{task.text}</span>
-            <button>タスク削除用ボタン</button>
+            <button onSubmit={handleDelete}>タスク削除用ボタン</button>
           </p>
           </div>
         </div>
