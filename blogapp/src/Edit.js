5a37daa8dc8ca7bad430b47096fa0c16559cab83
arahito1994 <artht0408@gmail.com>
2023-06-27 06:41:51 +0900

    編集ページのレイアウト

--- blogapp/src/Edit.js
+++ blogapp/src/Edit.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Edit = () => {
   return (
-    <div>Edit</div>
+    <form>
+      <label>
+        投稿内容
+        <span>: </span>
+      </label>
+      <input type="text"></input>
+      <button type="submit">編集</button>
+    </form>
   )
 }
 
