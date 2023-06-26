d26514ec86bb08edf67b82566c59bc248d02d861
arahito1994 <artht0408@gmail.com>
2023-06-27 06:40:52 +0900

    新規投稿ページのレイアウト

--- blogapp/src/NewPost.js
+++ blogapp/src/NewPost.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const NewPost = () => {
   return (
-    <div>NewPost</div>
+    <form>
+      <label>
+        新規投稿内容
+        <span>: </span>
+      </label>
+      <input type="text"></input>
+      <button type="submit">投稿</button>
+    </form>
   )
 }
 
